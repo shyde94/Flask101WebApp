3a4da0c19a5b7810e63b94cc2163e2d162ebaebe
Shide <shidefoo@gmail.com>
2018-02-05 20:54:10 +0800

    commented out @app.after_request.

--- app.py
+++ app.py
@@ -93,13 +93,13 @@ def signOut():
 	session.clear()
 	return redirect(url_for('index'))
 
-
+'''
 # Ensure responses aren't cached
 @app.after_request
 def after_request(response):
     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
     return response
-
+'''
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0')
